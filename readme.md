ee795b92a8222cc7fa8e2568a234b52f3ada3937
abidkyo <kyotasuna95@gmail.com>
2023-12-06 06:48:56 +0100

    ENH day 06

--- readme.md
+++ readme.md
@@ -9,6 +9,7 @@
 | [02](https://adventofcode.com/2023/day/2) | Parse digits in string    | [day02.py](src/day02.py) |
 | [03](https://adventofcode.com/2023/day/3) |                           | [day03.py](src/day03.py) |
 | [04](https://adventofcode.com/2023/day/4) | Winning cards and numbers | [day04.py](src/day04.py) |
+| [06](https://adventofcode.com/2023/day/6) | Lower and upper bounds    | [day05.py](src/day05.py) |
 
 ### Note
 
---
+++ src/day06.py
@@ -0,0 +1,62 @@
+#!/usr/bin/env python3
+
+"""
+Find the lower and upper bound.
+
+Timing:
+python3 = 1607 ms
+pypy3 = 100 ms
+"""
+
+from math import prod
+from aoc import integers, read_input
+
+# ------------------------------------------------------------------------------
+
+
+def count_win(t: tuple, d: tuple) -> int:
+    lb = 0
+    ub = 0
+
+    # find first win from 1
+    for t_button in range(1, t):
+        d_travel = t_button * (t - t_button)
+
+        if d_travel > d:
+            lb = t_button
+            break
+
+    # find first win from t-1
+    for t_button in reversed(range(lb, t)):
+        d_travel = t_button * (t - t_button)
+
+        if d_travel > d:
+            ub = t_button
+            break
+
+    return ub - lb + 1
+
+
+def solve(day=6, test=False):
+    txt = read_input(day, test).splitlines()
+    data = [t.split(":")[1] for t in txt]
+
+    time, distance = [integers(d) for d in data]
+    part1 = prod(count_win(time[i], distance[i]) for i in range(len(time)))
+
+    time, distance = [integers(d.replace(" ", "")) for d in data]
+    part2 = count_win(time[0], distance[0])
+
+    return part1, part2
+
+
+# ------------------------------------------------------------------------------
+
+# res = solve(test=True)
+# assert res == (288, 71503)
+
+res = solve()
+print(*res)
+assert res == (2065338, 34934171)
+
+# ------------------------------------------------------------------------------
